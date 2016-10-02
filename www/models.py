0562b9e04280b6c6754b0b780f925fba2a78df0b
lanyutao <lanyutaoem@163.com>
2016-10-02 10:18:07 +0800

    add model

---
+++ www/models.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time, uuid
+
+from transwarp.db import next_id
+from transwarp.orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+class User(Model):
+	__table__ = 'users'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	email = StringField(updatable=False, ddl='varchar(50)')
+	passwd = StringField(ddl='varchar(50)')
+	admin = BooleanField()
+	name = StringField(ddl='varchar(50)')
+	image = StringField(ddl='varchar(500)')
+	created_at = FloatField(updatable=False, default=time.time)
+
+class Blog(Model):
+	__table__ = 'blogs'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	user_id = StringField(updatable=False, ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	name = StringField(ddl='varchar(50)')
+	summary = StringField(ddl='varchar(200)')
+	content = TextField()
+	created_at = FloatField(updatable=False, default=time.time)
+
+class Comment(Model):
+	__table__ = 'comments'
+
+	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+	blog_id = StringField(updatable=False, ddl='varchar(50)')
+	user_id = StringField(updatable=False, ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(50)')
+	content = TextField()
+	created_at = FloatField(updatable=False, default=time.time)
+		
+
+		
+		
