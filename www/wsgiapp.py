6e169b879757796f692d87444b95a25f66e9ac5d
lanyutao <lanyutaoem@163.com>
2016-10-22 22:09:14 +0800

    add wsgi app,test template

---
+++ www/wsgiapp.py
@@ -0,0 +1,28 @@
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import os
+
+from transwarp import db
+from transwarp.web import WSGIApplication, Jinja2TemplateEngine
+
+from config import configs
+
+# init db:
+db.create_engine(**configs.db)
+
+# init wsgi app:
+wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
+
+template_engine = Jinja2TemplateEngine(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+
+wsgi.template_engine = template_engine
+
+import urls
+
+wsgi.add_module(urls)
+
+if __name__ == '__main__':
+    wsgi.run(9000)
