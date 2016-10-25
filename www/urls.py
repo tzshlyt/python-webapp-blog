a624a7b10604f0334b49ba6afe806fcb2e1ec9ec
lanyutao <lanyutaoem@163.com>
2016-10-26 00:44:21 +0800

    add home page

--- www/urls.py
+++ www/urls.py
@@ -7,10 +7,17 @@ from transwarp.web import get, view
 from models import User, Blog, Comment
 
 
-@view('test_users.html')
+# @view('test_users.html')
+# @get('/')
+# def test_users():
+#     users = User.find_all()
+#     print ">>>>>>>>> urls   "
+#     print users
+#     return dict(users=users)
+
+@view('blogs.html')
 @get('/')
-def test_users():
-    users = User.find_all()
-    print ">>>>>>>>> urls   "
-    print users
-    return dict(users=users)
+def index():
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import logging
 logging.basicConfig(level=logging.INFO)
 
 import os
+import time
 
 from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
@@ -12,11 +15,26 @@ from config import configs
 # init db:
 db.create_engine(**configs.db)
 
+
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
 # init wsgi app:
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
 
 template_engine = Jinja2TemplateEngine(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
@@ -25,4 +43,6 @@ import urls
 wsgi.add_module(urls)
 
 if __name__ == '__main__':
-    wsgi.run(9000)
+    wsgi.run(9000, '0.0.0.0')
+else:
+    application = wsgi.get_wsgi_application()
